6e43826ee0932e1bb4faaf7f6cbf943e3114803d
Matt Dray <mwdray@gmail.com>
2023-01-17 08:17:48 +0000

    Update lookup data so ID is numeric

--- R/data.R
+++ R/data.R
@@ -1,13 +1,14 @@
 
-#' All Endpoint Resources
+#' All Resources and Endpoints
 #'
 #' IDs, names and URLs for all the resources for each endpoint that can be
 #' requested from the PokéAPI.
 #'
-#' @source PokéAPI <https://pokeapi.co/>.
+#' @source API by PokéAPI <https://pokeapi.co/>. Data from The Pokémon Company.
 #'
-#' @format A list of dataframes of length 48, which is the number of endpoints
-#'     available from PokéAPI. Each dataframe has columns \code{id}, \code{name}
-#'     (possibly) and \code{url}, and a number of rows equal to the total number
-#'     of resources for the given endpoint.
+#' @format A list of dataframes of length 48, which is one per endpoint
+#'     available from PokéAPI. Each dataframe has columns \code{id} (integer),
+#'     \code{name} (character) (possibly) and \code{url} (character), and a
+#'     number of rows equal to the total number of resources available for the
+#'     given endpoint.
 "resource_lookups"
